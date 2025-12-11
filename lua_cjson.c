439997333efd8fea1999ff3701209f63d57681bd
Wangchong Zhou <fffonion@gmail.com>
2025-12-11 18:03:32 +0800

    do not encode array null

--- lua_cjson.c
+++ lua_cjson.c
@@ -84,7 +84,7 @@
 #define DEFAULT_DECODE_ARRAY_WITH_ARRAY_MT 0
 #define DEFAULT_ENCODE_ESCAPE_FORWARD_SLASH 1
 #define DEFAULT_ENCODE_SKIP_UNSUPPORTED_VALUE_TYPES 0
-#define DEFAULT_DECODE_NULL_AS_NIL 0
+#define DEFAULT_decode_null_object_value_as_nil 0
 
 #ifdef DISABLE_INVALID_NUMBERS
 #undef DEFAULT_DECODE_INVALID_NUMBERS
@@ -169,7 +169,7 @@ typedef struct {
     int decode_max_depth;
     int decode_array_with_array_mt;
     int encode_skip_unsupported_value_types;
-    int decode_null_as_nil;
+    int decode_null_object_value_as_nil;
 } json_config_t;
 
 typedef struct {
@@ -393,11 +393,11 @@ static int json_cfg_encode_keep_buffer(lua_State *l)
 }
 
 /* Configures how to treat null when decoding */
-static int json_cfg_decode_null_as_nil(lua_State *l)
+static int json_cfg_decode_null_object_value_as_nil(lua_State *l)
 {
     json_config_t *cfg = json_arg_init(l, 1);
 
-    json_enum_option(l, 1, &cfg->decode_null_as_nil, NULL, 1);
+    json_enum_option(l, 1, &cfg->decode_null_object_value_as_nil, NULL, 1);
 
     return 1;
 }
@@ -493,7 +493,7 @@ static void json_create_config(lua_State *l)
     cfg->decode_array_with_array_mt = DEFAULT_DECODE_ARRAY_WITH_ARRAY_MT;
     cfg->encode_escape_forward_slash = DEFAULT_ENCODE_ESCAPE_FORWARD_SLASH;
     cfg->encode_skip_unsupported_value_types = DEFAULT_ENCODE_SKIP_UNSUPPORTED_VALUE_TYPES;
-    cfg->decode_null_as_nil = DEFAULT_DECODE_NULL_AS_NIL;
+    cfg->decode_null_object_value_as_nil = DEFAULT_decode_null_object_value_as_nil;
 
 #if DEFAULT_ENCODE_KEEP_BUFFER > 0
     strbuf_init(&cfg->encode_buf, 0);
@@ -896,7 +896,7 @@ static int json_encode(lua_State *l)
 /* ===== DECODING ===== */
 
 static void json_process_value(lua_State *l, json_parse_t *json,
-                               json_token_t *token);
+                               json_token_t *token, int decode_null_object_value_as_nil);
 
 static int hexdigit2int(char hex)
 {
@@ -1325,7 +1325,8 @@ static void json_parse_object_context(lua_State *l, json_parse_t *json)
 
         /* Fetch value */
         json_next_token(json, &token);
-        json_process_value(l, json, &token);
+        json_process_value(l, json, &token,
+                           json->cfg && json->cfg->decode_null_object_value_as_nil);
 
         /* Set key = value */
         lua_rawset(l, -3);
@@ -1372,7 +1373,7 @@ static void json_parse_array_context(lua_State *l, json_parse_t *json)
     }
 
     for (i = 1; ; i++) {
-        json_process_value(l, json, &token);
+        json_process_value(l, json, &token, 0);
         lua_rawseti(l, -2, i);            /* arr[i] = value */
 
         json_next_token(json, &token);
@@ -1391,7 +1392,7 @@ static void json_parse_array_context(lua_State *l, json_parse_t *json)
 
 /* Handle the "value" context */
 static void json_process_value(lua_State *l, json_parse_t *json,
-                               json_token_t *token)
+                               json_token_t *token, int decode_null_object_value_as_nil)
 {
     switch (token->type) {
     case T_STRING:
@@ -1412,7 +1413,7 @@ static void json_process_value(lua_State *l, json_parse_t *json,
     case T_NULL:
         /* In Lua, setting "t[k] = nil" will delete k from the table.
          * Hence a NULL pointer lightuserdata object is used instead */
-        if (json->cfg && json->cfg->decode_null_as_nil)
+        if (decode_null_object_value_as_nil)
             lua_pushnil(l);
         else
             lua_pushlightuserdata(l, NULL);
@@ -1449,7 +1450,7 @@ static int json_decode(lua_State *l)
     json.tmp = strbuf_new(json_len);
 
     json_next_token(&json, &token);
-    json_process_value(l, &json, &token);
+    json_process_value(l, &json, &token, 0);
 
     /* Ensure there is no more input left */
     json_next_token(&json, &token);
@@ -1537,7 +1538,7 @@ static int lua_cjson_new(lua_State *l)
         { "decode_invalid_numbers", json_cfg_decode_invalid_numbers },
         { "encode_escape_forward_slash", json_cfg_encode_escape_forward_slash },
         { "encode_skip_unsupported_value_types", json_cfg_encode_skip_unsupported_value_types },
-        { "decode_null_as_nil", json_cfg_decode_null_as_nil },
+        { "decode_null_object_value_as_nil", json_cfg_decode_null_object_value_as_nil },
         { "new", lua_cjson_new },
         { NULL, NULL }
     };
--- tests/agentzh.t
+++ tests/agentzh.t
@@ -340,9 +340,15 @@ local cjson = require "cjson"
 local json = [[{"key1":null,"key2":"value2"}]]
 local t = cjson.decode(json)
 print(t.key1 == cjson.null and "key1 is null" or "key1 is not null")
-cjson.decode_null_as_nil(true)
+cjson.decode_null_object_value_as_nil(true)
 local t2 = cjson.decode(json)
 print(t2.key1 == nil and "key1 is nil" or "key1 is not nil")
+
+-- array null are not changed
+local json_arr = [[[null, "value2"]]]
+local t = cjson.decode(json)
+print(t[1] == cjson.null and "index 1 is null" or "key1 is not null")
 --- out
 key1 is null
 key1 is nil
+index 1 is null
